378fb6e92ae7c3c8390961fcfc185a5ac8c82190
kishmat <kishmat17@gmail.com>
2020-08-07 21:50:44 +0545

    Updates

---
+++ precache-manifest.f329f9b4026de4ea35e0623528c1d03c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba79332113fe74688ca79732d1a112c",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "906c20c4b648d4f9c59c",
+    "url": "/covid-tracker/static/css/main.417a3f37.chunk.css"
+  },
+  {
+    "revision": "bf755917833d8841d256",
+    "url": "/covid-tracker/static/js/2.4f2779dd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.4f2779dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906c20c4b648d4f9c59c",
+    "url": "/covid-tracker/static/js/main.a0ffd325.chunk.js"
+  },
+  {
+    "revision": "46466abd9176ac89141b",
+    "url": "/covid-tracker/static/js/runtime-main.27f7c4e2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
